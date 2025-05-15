6015339b6f04dadd89ebe752d35670c5310ca441
Evgeniy Maslov <ruwell.masloves@yandex.ru>
2025-05-15 17:58:34 +0300

    feat: Реализовал функции добавления и удаления карточек.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Masloves/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
-
 // @todo: DOM узлы
-
 // @todo: Функция создания карточки
-
 // @todo: Функция удаления карточки
-
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector(".places__list");
+
+function addCard({ name, link, alt }) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.setAttribute("src", link);
+  cardImage.setAttribute("alt", alt);
+
+  cardElement.querySelector(".card__title").textContent = name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+
+  return cardElement;
+};
+
+function deleteCard() {
+  const cards = this.parentElement;
+  cards.remove();
+};
+
+initialCards.map(addCard).forEach(card => placesList.append(card));
