cf66b7821e2cb3ce8d48ddcd28255fac7ea62952
Evgeniy Maslov <ruwell.masloves@yandex.ru>
2025-05-16 15:49:06 +0300

    fix: Внес правки после ревью.

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,31 +2,25 @@ const initialCards = [
     {
       name: "Токио",
       link: "https://images.unsplash.com/photo-1746555702228-5c4f5436d4b7?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D",
-      alt: "Улица ведущая к токийскому небоскребу skytree.",
     },
     {
       name: "Эдинбург",
       link: "https://plus.unsplash.com/premium_photo-1699566448055-671c8dbcc7ee?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D",
-      alt: "Мощеная улица с часовой башней на заднем плане.",
     },
     {
       name: "Чикаго",
       link: "https://images.unsplash.com/photo-1746513420182-56a5a1f50034?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      alt: "Городская улица с высокими зданиями и автомобилями.",
     },
     {
       name: "Южный Тироль",
       link: "https://images.unsplash.com/photo-1741893041975-94a0e8656209?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      alt: "Красивое горное озеро Брайес с причалом.",
     },
     {
       name: "Венеция",
       link: "https://images.unsplash.com/photo-1741648711665-e1a8003b7891?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      alt: "Большой канал и базилика Санта-Мария-делла-Салюте.",
     },
     {
       name: "Норвегия",
       link: "https://images.unsplash.com/photo-1724271362937-391a150db603?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      alt: "Лодка, пришвартованная к причалу, на фоне горы.",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 const cardsContainer = document.querySelector(".places__list");
 
-function createCard({ name, link, alt }, deleteCard) {
+function createCard({ name, link }, deleteCard) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
   const cardImage = cardElement.querySelector(".card__image");
   cardImage.setAttribute("src", link);
-  cardImage.setAttribute("alt", alt);
+  cardImage.setAttribute("alt", name);
 
   cardElement.querySelector(".card__title").textContent = name;
 
